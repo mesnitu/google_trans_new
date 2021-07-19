8984314034e6b954fae0b539700a443096616d39
Daniel Sousa <mesnitu@users.noreply.github.com>
2021-07-19 14:39:30 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='google_trans_new_fix',
+    version='0.0.1',
+    packages=['google_trans_new_fix'],
+    url='',
+    license='MIT',
+    author='lushan88a',
+    author_email='',
+    description='temp bug fix https://github.com/lushan88a/google_trans_new'
+)
